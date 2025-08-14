abbf662ecf37c6bf2bf2045de648cd3c5f3c94c6
SuprotikPanja <suprotikpanja@gmail.com>
2025-08-14 22:54:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (43.05%) - LeetSync

--- 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.java
+++ 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.java
@@ -1,34 +1,25 @@
 class Solution {
     public boolean isAlienSorted(String[] words, String order) {
-        //using hashmap
-        Map <Character,Integer> orderMap = new HashMap<>();
-        // Step 1: Create a map to store the order of each letter
-        for(int i = 0; i < order.length(); i++){       //Filling the order with position in HashMap
-            orderMap.put(order.charAt(i),i);
-        } 
+        
+        int[] alienOrder = new int[26];
+        for (int i = 0; i < order.length(); i++) {
+            alienOrder[order.charAt(i) - 'a'] = i; // Store index position
+        }
 
+        for(int i=0; i<words.length-1; i++){
+            for(int j=0; j<words[i].length(); j++){
+                if(j>=words[i+1].length()) return false;//like at i->apple and at i+1->app
+                int currentChar=words[i].charAt(j);
+                int nextChar=words[i+1].charAt(j);
+                if(currentChar!=nextChar){
+                    int currentCharIdx=currentChar-'a';
+                    int nextCharIdx=nextChar-'a';
 
-        //  Step 2: Compare each word with the next one in the list
-        for(int i = 0; i < words.length-1; i++){
-            // Compare characters of words[i] and words[i+1] one by one
-            for(int j = 0; j < words[i].length(); j++){
-
-                 // Case 1: The second word is shorter but is a prefix of the first
-                if(j >= words[i+1].length()) return false;   //batman, bat
-
-                 // Case 2: Characters at position j are different
-                if(words[i].charAt(j) != words[i+1].charAt(j)){
-
-                     // Find the position of both characters in alien order
-                    int currentLetter = orderMap.get(words[i].charAt(j));
-                    int nextLetter = orderMap.get(words[i+1].charAt(j));
-                
-                // If the next word's letter comes before the current word's letter
-                if (nextLetter < currentLetter) return false;
-                else break;    // If correct order found, no need to check further characters
+                    if(alienOrder[currentCharIdx] > alienOrder[nextCharIdx]) return false;
+                    else break;
                 }
             }
         }
-    return true;    // Step 3: If no rules were broken, the list is correctly sorted
+        return true;
     }
 }
